c462068979b8a47a581bde87db292abdd56404a0
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-04-08 01:09:32 +0530

    final

---
+++ mystaticcard/src/components/JoinUs.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const JoinUS = (props)=>{
+
+    return (
+        <div id='buttondiv'>
+            <button id='btnJoin' > {props.heading} </button>
+        </div>
+    );
+
+}
+
+export default JoinUS ;
---
+++ mystaticcard/src/components/Settings.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+const Settings = ()=>{
+
+    return (
+        <div id='buttondiv'>
+             <button id='btnSettings'>SETTINGS</button>
+         </div>
+    );
+
+}
+export default Settings;
